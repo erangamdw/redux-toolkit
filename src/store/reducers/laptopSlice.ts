34042c858a6ebfe21f3df169815b0a002ffb0c5b
erangamdw <erangamdw@gmail.com>
2023-05-28 02:43:17 +0530

    configured the store

---
+++ src/store/reducers/laptopSlice.ts
@@ -0,0 +1,30 @@
+import {createSlice, PayloadAction} from "@reduxjs/toolkit";
+
+interface laptopState {
+    count: number;
+    brand: string[]; // Example type for `computers`
+}
+
+const initialState: laptopState = {
+    count: 10,
+    brand: ['Dell', 'Hp']
+}
+
+const laptopSlice = createSlice({
+    name: "laptop",
+    initialState,
+    reducers:{
+        addLaptop:(state, action: any)=>({
+            ...state,
+            count: state.count + action.payload,
+        }),
+        removeLaptop:(state, action: any)=>({
+            ...state,
+            count: state.count - action.payload,
+        })
+    }
+}
+)
+
+export const {addLaptop, removeLaptop} = laptopSlice.actions;
+export default laptopSlice.reducer
---
+++ src/store/store.ts
@@ -0,0 +1,12 @@
+import { configureStore } from "@reduxjs/toolkit";
+import numberSliceReducer from "./reducers/numberSlice";
+import laptopSliceReducer from "./reducers/laptopSlice";
+
+const store = configureStore({
+    reducer:{
+        numberSlice: numberSliceReducer,
+        laptopSlice: laptopSliceReducer
+    }
+})
+
+export default store;
