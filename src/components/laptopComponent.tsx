8db0bcd943c124a63110aeed478c1d57ff5e40ec
erangamdw <erangamdw@gmail.com>
2023-05-28 21:10:33 +0530

    dispatch add data with using prepare method in the slice, so we dont need to worry about the structure of the object

--- src/components/laptopComponent.tsx
+++ src/components/laptopComponent.tsx
@@ -14,30 +14,44 @@ const LaptopComponent = () => {
   const ram = useRef<HTMLInputElement>(null);
   const hdd = useRef<HTMLInputElement>(null);
 
-  const addLaptopHandler = () => {
-    const newLaptop = {
-      id: nanoid(),
-      price: parseInt(price.current?.value || "0"),
-      specs: {
-        cpu: cpu.current?.value ?? "",
-        gen: parseInt(gen.current?.value || "0"), // Parse gen as an integer
-        ram: ram.current?.value ?? "",
-        hdd: hdd.current?.value ?? "",
-      },
-    };
+//   const addLaptopHandler = () => {
+//     const newLaptop = {
+//       id: nanoid(),
+//       price: parseInt(price.current?.value || "0"),
+//       specs: {
+//         cpu: cpu.current?.value ?? "",
+//         gen: parseInt(gen.current?.value || "0"), // Parse gen as an integer
+//         ram: ram.current?.value ?? "",
+//         hdd: hdd.current?.value ?? "",
+//       },
+//     };
   
-    if (
-      newLaptop.price !== undefined &&
-      newLaptop.specs.cpu !== undefined &&
-      !isNaN(newLaptop.specs.gen) && // Check if gen is a valid number
-      newLaptop.specs.ram !== undefined &&
-      newLaptop.specs.hdd !== undefined
-    ) {
-      dispatch(addLaptop(newLaptop));
-    } else {
-      // Handle the case when any of the values are missing or invalid
-      console.log('Please enter all the laptop details.');
-    }
+//     if (
+//       newLaptop.price !== undefined &&
+//       newLaptop.specs.cpu !== undefined &&
+//       !isNaN(newLaptop.specs.gen) && // Check if gen is a valid number
+//       newLaptop.specs.ram !== undefined &&
+//       newLaptop.specs.hdd !== undefined
+//     ) {
+//       dispatch(addLaptop(newLaptop));
+//     } else {
+//       // Handle the case when any of the values are missing or invalid
+//       console.log('Please enter all the laptop details.');
+//     }
+//   };
+
+//we can dispatch the addlaptop action when press the btn. we have to parse the data to the dispatch,
+//  if we use prepare with the slice, we don't need to worry about the object, we only need to parse the values same as variables
+const addLaptopHandler = () => {
+    dispatch(
+      addLaptop(
+        price.current?.value,
+        cpu.current?.value,
+        gen.current?.value,
+        ram.current?.value,
+        hdd.current?.value // Use optional chaining operator
+      )
+    );
   };
   
   
--- src/store/reducers/laptopSlice.ts
+++ src/store/reducers/laptopSlice.ts
@@ -1,4 +1,6 @@
 import {createSlice, PayloadAction} from "@reduxjs/toolkit";
+import { nanoid } from '@reduxjs/toolkit'
+//nanoid use to generate a id
 
 interface LaptopState {
     id: any;
@@ -30,15 +32,32 @@ const laptopSlice = createSlice({
     name: "laptop",
     initialState,
     reducers:{
-        addLaptop: (state, action: PayloadAction<LaptopState>) => {
-            state.push(action.payload);
-        },
+        //if we want to structure the object when we parsing the values, we can destrucure the reducer to two partsand make prepare 
+        // the reducer action using the prepare and return that object to the payload
+        addLaptop: {
+            reducer: (state, action: PayloadAction<LaptopState>) => {
+                state.push(action.payload);
+            },
+            prepare:(price, cpu, gen, ram, hdd)=>{
+                return{
+                    payload:{
+                        id: nanoid(),
+                        price: parseInt(price || "0"),
+                        specs: {
+                        cpu: cpu ?? "",
+                        gen: parseInt(gen || "0"), // Parse gen as an integer
+                        ram: ram ?? "",
+                        hdd: hdd ?? "",
+                    }
+                }
+            }
+        }
         // removeLaptop:(state, action: any)=>({
         //     ...state,
         //     count: state.count - action.payload,
         // })
     }
-}
+}}
 )
 
 //selectors
