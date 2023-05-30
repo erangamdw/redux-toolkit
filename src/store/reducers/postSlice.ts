a6a1846f761d030d9031d734a0258b6e15898d2c
erangamdw <erangamdw@gmail.com>
2023-05-30 17:33:03 +0530

    update

--- src/store/reducers/postSlice.ts
+++ src/store/reducers/postSlice.ts
@@ -16,9 +16,9 @@ const initialState: postState = {
 //thunk waladi dispatch karaddi call karanneth meka     // dispatch karaddi parameter ekak ganna/ parse karanna onenm eka ganne async eke () walata
 export const getPosts = createAsyncThunk('getPosts', async ()=>{
     //try catch one na
-const res = await fetch('https://jsonplaceholder.typicode.com/posts')
+const res = await fetch('https://jsonplaceholder.typicode.csom/posts')
     const data = await res.json();
-    if(data){
+    if(Array.isArray(data)){
         return data;
     }else{
         return{err: "some error"}
@@ -43,8 +43,13 @@ const postSlice = createSlice({
         })
         builder.addCase(getPosts.fulfilled, (state:any, action:PayloadAction<postState>)=>{ // itapase normal reducer function eka liyanawa
             //immer library use wena nisa spread karanna one na
-            state.loading = "completed";
-            state.data = action.payload
+            if(action.payload.error){
+                state.loading = 'failed'
+            }else{
+                state.loading = "completed";
+                state.data = action.payload
+            }
+            
         })
         //reject / fail unoth
         builder.addCase(getPosts.rejected, (state: any, action:PayloadAction<postState>)=>{
